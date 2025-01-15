d7a91e1cecae50a1eed1879c0bd2fb2fd3c3f5b0
128bitpotato <skickligt01.flagig@icloud.com>
2025-01-15 16:22:09 +0100

    testing cell.draw_move

--- src/main.py
+++ src/main.py
@@ -4,21 +4,19 @@ from cell import Cell
 def main():
     win = Window(800, 600)
 
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c2 = Cell(win)
+    c2.has_left_wall, c2.has_right_wall = False, False
+    c2.draw(100, 50, 150, 100)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c3 = Cell(win)
+    c3.has_left_wall = False
+    c3.draw(150, 50, 200, 100)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    c1.draw_move(c3)
 
     win.wait_for_close()
 
