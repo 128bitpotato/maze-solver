47fae62fdd615119126d3e14272df9d23fef5752
128bitpotato <skickligt01.flagig@icloud.com>
2025-01-15 09:58:07 +0100

    constructor updated

--- src/window.py
+++ src/window.py
@@ -2,16 +2,24 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__root = Tk()
+        self.__root.title("Maze solver")
 
-        self.root = Tk()
-        self.root.title("Maze solver")
-
-        self.canvas = Canvas()
+        self.canvas = Canvas(self.__root, height=height, width=width)
         self.canvas.pack()
 
         self.running = False
-    
-    
 
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running is True:
+            self.redraw()
+
+    def close(self):
+        self.running = False
