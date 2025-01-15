9712e709b57bdaa8d137d6eb7032f75c9e6d43b5
128bitpotato <skickligt01.flagig@icloud.com>
2025-01-15 14:26:52 +0100

    update to test graphics

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
-from window import Window
+from window import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+
+    point1 = Point(10, 10)
+    point2 = Point(50, 50)
+    line1 = Line(point1, point2)
+    win.draw_line(line1, "red")
+
+    point3 = Point(100, 100)
+    point4 = Point(100, 150)
+    line2 = Line(point3, point4)
+    win.draw_line(line2, "black")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
