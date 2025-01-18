256213eb6dc2ec4b9513804f684d412599ed701d
128bitpotato <skickligt01.flagig@icloud.com>
2025-01-18 11:07:58 +0100

    added unittests

--- src/tests.py
+++ src/tests.py
@@ -39,6 +39,15 @@ class Tests(unittest.TestCase):
             False,
         )
 
+    def test_visited_reset(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEquals(
+            m1._cells[0][3].visited,
+            False,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
